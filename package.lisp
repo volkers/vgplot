972be14a2a03f32489573320ac0a718fc667d5aa
Volker Sarodnick <volkersar@gmx.net>
2013-04-14 16:39:49 +0200

    vgplot:test replaced by vgplot:demo more convenient demo.

--- package.lisp
+++ package.lisp
@@ -3,5 +3,6 @@
 (defpackage #:vgplot
   (:use :cl)
   (:import-from :ltk :do-execute)
+  (:import-from :cl-ppcre :regex-replace-all)
   (:import-from :cl-fad :with-output-to-temporary-file)
-  (:export :format-plot :close-plot :close-all-plots :new-plot :plot :test :range))
+  (:export :format-plot :close-plot :close-all-plots :new-plot :plot :demo :range))
--- vgplot.asd
+++ vgplot.asd
@@ -5,6 +5,6 @@
   :description "Interface to gnuplot"
   :author "Volker Sarodnick <volkersar@gmx.net>"
   :license "GPL"
-  :depends-on (#:ltk #:cl-fad)
+  :depends-on (#:ltk #:cl-ppcre #:cl-fad)
   :components ((:file "package")
                (:file "vgplot")))
