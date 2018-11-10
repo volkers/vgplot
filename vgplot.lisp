3467c833caf1f0dba820e6079c4c7f4bef750f80
Volker Sarodnick <volkersar@gmx.net>
2018-11-10 11:09:05 +0100

    Contribution comment added.

--- vgplot.lisp
+++ vgplot.lisp
@@ -16,6 +16,10 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Main author: Volker Sarodnick
+
+    Contributions: Lewis Grozinger
 |#
 
 (in-package #:vgplot)
